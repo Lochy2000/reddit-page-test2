acb209637412af2bcb0686ee1609ce76b96b213d
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2024-11-08 13:06:29 +0000

    registered category model to admin panel in django

--- redditclone/posts/admin.py
+++ redditclone/posts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','created_at')
+    search_fields =  ('name',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','author', 'category', 'create_at')
+    list_filter = ('category', 'create_at')
+    search_fields = ('title', 'content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('content',)
